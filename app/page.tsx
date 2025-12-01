f409b1b75e83ed525bc12f5fd986c019b9278711
David Fredkove <spot4dave@gmail.com>
2025-12-01 16:05:06 -0600

    adjust layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import { Autocomplete } from "./components/Autocomplete"
 
 export default function Home() {
   return (
-    <div className="flex min-h-[75vh] items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+    <div className="flex min-h-screen flex-col items-center justify-start bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex w-full max-w-3xl flex-col gap-6 px-16 py-12 bg-white dark:bg-black">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Word Autocomplete Search
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
@@ -15,7 +15,7 @@ export default function Home() {
             auto-complete as you type.
           </p>
         </div>
-        <div className="mt-8 w-full max-w-md">
+        <div className="w-full max-w-md">
           <Autocomplete />
         </div>
       </main>
